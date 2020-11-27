20da53d2d66dbf1e1b68f5519735a9a16e9f45e6
admin <admin@MORENO-P.onlineformapro.org>
2020-11-27 08:37:10 +0100

    first commit

---
+++ exo10.php
@@ -0,0 +1,4 @@
+<?php
+//Créer tableau associatif des personnes présentes sur votre îlot. Utilisez pour les clés "person1", "perosn2",...
+
+//Code à faire ici
---
+++ exo9.php
@@ -0,0 +1,4 @@
+<?php
+//Créer un tableau classique comme en JS, qui contient les noms des personnes sur votre ilôt.
+
+//Code à faire ici
